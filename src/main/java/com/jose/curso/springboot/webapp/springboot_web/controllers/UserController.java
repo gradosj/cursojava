91fcc27edd8ba54b8f413f570ab0557af5f84bc4
Jose <gradosj@gmail.com>
2025-12-11 10:15:27 +0100

    Mostrar lista usuarios por html

--- src/main/java/com/jose/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/jose/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.jose.curso.springboot.webapp.springboot_web.controllers.models.User;
-
-
 @Controller
 public class UserController {
 
